102c6b8d5b4491636b7a4515379b6212c755ed96
Jose Marcelo Simao <josesimao@guarulhos.sp.gov.br>
2023-05-24 18:08:05 -0300

    first commit

---
+++ src/main/java/br/com/josemarcelo/SpringBootExcel/SpringBootExcelApplication.java
@@ -0,0 +1,13 @@
+package br.com.josemarcelo.SpringBootExcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExcelApplication.class, args);
+	}
+
+}
